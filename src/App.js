98c2daecace30bddd551dd7830c1d9d4e7a47f85
SoniaFaMa <101564312+SoniaFaMa@users.noreply.github.com>
2024-03-01 21:16:52 +0100

    first container L

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          holi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+ <div className='container-L'>
+  <div className='header'>
+    <p>Header</p>
+  </div>
+
+    <div className='one'>
+      <p>1</p>
     </div>
-  );
+
+
+    <div className='two'>
+    <p>2</p>
+    </div>
+
+    <div className='three'>
+    <p>3</p>
+    </div>
+
+    <div className='footer1'>
+      <p>Footer1</p>
+      </div>
+    <div className='footer2'>
+    <p>Footer2</p>
+      </div>
+ 
+
+
+
+
+
+ </div> 
+ 
+  )
 }
+
+
 
 export default App;
